2f4306ea3b0dead077f81288ce8ee651725ab652
hongyouqin <yang.qq123@163.com>
2021-05-25 17:12:49 +0800

    修改参数

--- scripts/test.sh
+++ scripts/test.sh
@@ -33,7 +33,6 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python ./apps/eval.py \
     --hg_down 'ave_pool' \
     --norm 'group' \
     --norm_color 'group' \
-    --num_views 3 \
     --loadSize 1024 \
     --test_folder_path ${TEST_FOLDER_PATH} \
     --load_netG_checkpoint_path ${CHECKPOINTS_NETG_PATH} \
